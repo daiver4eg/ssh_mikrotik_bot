8b837158cad7958210ba29ddc4660834d61f8a0a
Vasua Pupkin <you@gmail.com>
2023-08-09 22:00:44 +0300

    add empty config file

---
+++ config.py
@@ -0,0 +1,7 @@
+#@BotFather
+TOKEN = ""
+#Credentials for SSH connection to device(router)
+USER = ''
+SECRET = ''
+#ID your telegramm chat or group. You can use @myidbot    /getgroupid@myidbot for take ID
+CHANNEL_TLG = ""
